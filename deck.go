9cad131f89a25dabbf0ff5cd3da3cc68e1a9debd
dilloncooper15 <dilloncooper15@gmail.com>
2018-10-12 12:41:46 -0400

    Consolidated deal.go by moving 'func deal()' to main.go

--- deck.go
+++ deck.go
@@ -83,6 +83,15 @@ func newDeckFromFile(filename string) deck {
 }
 
 /**
+* Given a deck, and number of cards, returns a deck containing the desired number
+* of cards. A second deck is returned containing the remaining cards.
+**/
+func deal(d deck, handSize int) (deck, deck) {
+
+	return d[:handSize], d[handSize:]
+}
+
+/**
 * A Receiver Function that randomly reorganizes the order of a deck.
 **/
 func (d deck) shuffle() {
